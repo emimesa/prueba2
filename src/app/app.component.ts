54e20976a90e8ed1d814eaa58d89eb904b7c2f3b
emimesa <emilianomesa@yahoo.com.ar>
2021-05-25 11:41:56 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto061';
+  resultado: string="";
+
+  constructor(private fb: FormBuilder) { }
+
+  formularioContacto = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(10)]],
+    mail: ['', [Validators.required, Validators.email]],
+    mensaje: ['', [Validators.required, Validators.maxLength(500)]]
+  });
+
+  submit() {
+    if (this.formularioContacto.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
+  }
+
 }
